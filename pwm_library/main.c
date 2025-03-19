c57b7c69648e02bf3dc87f296c8aa28a4f8400a4
Marcus Loyd <Marcusloyd24@gmail.com>
2025-03-19 19:21:29 -0400

    Initial commit for motor control

---
+++ pwm_library/main.c
@@ -0,0 +1,150 @@
+/**
+ * Copyright (c) 2015 - 2018, Nordic Semiconductor ASA
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form, except as embedded into a Nordic
+ *    Semiconductor ASA integrated circuit in a product or a software update for
+ *    such product, must reproduce the above copyright notice, this list of
+ *    conditions and the following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ * 
+ * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ * 
+ * 4. This software, with or without modification, must only be used with a
+ *    Nordic Semiconductor ASA integrated circuit.
+ * 
+ * 5. Any software provided in binary form under this license must not be reverse
+ *    engineered, decompiled, modified and/or disassembled.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+/** @file
+ * @defgroup pwm_example_main main.c
+ * @{
+ * @ingroup pwm_example
+ *
+ * @brief  PWM Example Application main file.
+ *
+ * This file contains the source code for a sample application using PWM.
+ *
+ *
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "nrf.h"
+#include "app_error.h"
+#include "bsp.h"
+#include "nrf_delay.h"
+#include "app_pwm.h"
+
+APP_PWM_INSTANCE(PWM1,1);                   // Create the instance "PWM1" using TIMER1.
+
+static volatile bool ready_flag;            // A flag indicating PWM status.
+
+#define motor_switch_pin 27
+
+void pwm_ready_callback(uint32_t pwm_id)    // PWM callback function
+{
+    ready_flag = true;
+}
+
+void enable_pwm() 
+{
+    ret_code_t err_code;
+
+    /* 2-channel PWM, 200Hz, output on DK LED pins. */
+    app_pwm_config_t pwm1_cfg = APP_PWM_DEFAULT_CONFIG_2CH(5000L, NRF_GPIO_PIN_MAP(0,7), BSP_LED_1);
+
+    /* Switch the polarity of the second channel. */
+    pwm1_cfg.pin_polarity[1] = APP_PWM_POLARITY_ACTIVE_HIGH;
+    err_code = app_pwm_init(&PWM1,&pwm1_cfg,pwm_ready_callback);
+    APP_ERROR_CHECK(err_code);
+    app_pwm_enable(&PWM1);
+    nrf_delay_ms(10);
+}
+
+void disable_pwm () 
+{
+    app_pwm_uninit(&PWM1);
+    nrf_delay_ms(10);
+}
+
+void motor_switch_enable () {
+    nrf_gpio_pin_set(motor_switch_pin);
+}
+
+void motor_switch_disable() {
+    nrf_gpio_pin_clear(motor_switch_pin);
+}
+
+int main(void)
+{
+    ret_code_t err_code;
+
+    /* Initialize and enable PWM. */
+    // err_code = app_pwm_init(&PWM1,&pwm1_cfg,pwm_ready_callback);
+    // APP_ERROR_CHECK(err_code);
+    // disable_pwm();
+    // enable_pwm();
+    uint32_t value;
+
+    // configures the GPIO pin to output
+    nrf_gpio_cfg_output(motor_switch_pin);
+
+    //sets the GPIO output to high
+    motor_switch_enable();
+
+    while (true)
+    {
+        // err_code = app_pwm_init(&PWM1,&pwm1_cfg,pwm_ready_callback);
+        // APP_ERROR_CHECK(err_code);
+        //app_pwm_enable(&PWM1);   
+
+        enable_pwm();
+        motor_switch_enable();
+        
+        for (uint8_t i = 0; i < 40; ++i)
+        {
+            value = (i < 20) ? (i * 5) : (100 - (i - 20) * 5);
+
+            ready_flag = false;
+            /* Set the duty cycle - keep trying until PWM is ready... */
+            while (app_pwm_channel_duty_set(&PWM1, 0, value) == NRF_ERROR_BUSY);
+
+            /* ... or wait for callback. */
+            while (!ready_flag);
+            APP_ERROR_CHECK(app_pwm_channel_duty_set(&PWM1, 1, value));
+            nrf_delay_ms(25);
+        }
+
+        disable_pwm();
+        motor_switch_disable();
+        nrf_pwr_mgmt_run();
+        nrf_delay_ms(10000);
+
+    }
+
+}
+
+
+/** @} */
